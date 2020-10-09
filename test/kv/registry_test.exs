767e49e298fa165fef5ca962585dbef94d2b32b6
adrianbaertschi <adrian.baertschi@zuehlke.com>
2020-10-09 12:15:02 +0800

    Chapter 6: ETS

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,9 +1,9 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
   test "spawns bucket", %{registry: registry} do
@@ -29,6 +29,18 @@ defmodule KV.RegistryTest do
 
     # Crash the bucket
     Agent.stop(bucket, :shutdown)
+
+    # Do a call to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "bucket can crash at any time", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    Agent.stop(bucket, :shutdown)
+
+    catch_exit KV.Bucket.put(bucket, "milk", 3)
   end
 end
